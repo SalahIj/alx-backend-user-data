9ff2f38b675cb1dcb92ceda4d902b69a936c8882
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-06-04 15:17:03 +0000

    Task_1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,15 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """the get function"""
+    abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """The forbidden function"""
+    abort(403)
